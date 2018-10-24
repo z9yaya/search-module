101b4e38610f88b5830225a8c313bc78e505b315
Yannick Mansuy <ze_yaya@msn.com>
2018-10-25 07:49:37 +1000

    Added increment id function and cleaned up configure

--- src/search.js
+++ src/search.js
@@ -107,50 +107,43 @@ var SearchModule = {};
 			var this_table = $(_tables._raw_objects[i_tables]);
 			if (this_table.prop("id") == "") {
 				var temp_table_id = "AS_t"+i_tables;
+				temp_table_id = this.incrementID(temp_table_id);
 				this_table.prop("id", temp_table_id);
 			} else {
 				var temp_table_id = this_table.prop("id");
 			}
 			_tables["id"][temp_table_id] = {};
 			_tables["id"][temp_table_id]["DOM_object"] = this_table;
+			_tables["id"][temp_table_id]["id"] = temp_table_id;
 			_tables["id"][temp_table_id]["page_number"] =  this_table.data("page_number");
 			_tables["id"][temp_table_id]["search_URI"] = this_table.data("search_uri");
 			_tables["id"][temp_table_id]["rows"] = this_table.find("tbody tr");
+			_tables["id"][temp_table_id]["headers"] = ""; //assigned value on 132
+			_tables["id"][temp_table_id]["paginations"] = "";
+
 			var temp_headers_cache = this_table.find("thead > tr > td");
 			var temp_headers_output = Array();
 			var temp_rows_count = _tables["id"][temp_table_id]["rows"].length;
 			var temp_form_cache = this_table.parent().find("form.activeSearch_form");
 			var temp_pagination_cache = this_table.parent().find(".activeSearch_pagination");
 			var temp_pagination_output = Array();
+
 			for (var i_headers = 0; i_headers < temp_headers_cache.length; i_headers++) {
 					var this_header = $(temp_headers_cache[i_headers]);
-					temp_headers_output.push({"column_index": this_header.data("column_index"), "link": this_header.data("link"), "link_URI": this_header.data("link_uri"), "link_index": this_header.data("link_index")});
+					temp_headers_output.push(this.configureHeader(this_header));
 			}
+
 			_tables["id"][temp_table_id]["headers"] = temp_headers_output;
 			for (var i_rows = 0; i_rows < temp_rows_count; i_rows++) {
 				$(_tables["id"][temp_table_id]["rows"][i_rows]).attr("data-row_index", i_rows);
 			}
+
 			for (var i_forms = 0; i_forms < temp_form_cache.length; i_forms++) {
 				var this_form = $(temp_form_cache[i_forms]);
-				if (this_form.prop("id") == "") {
-					var temp_form_id = temp_table_id+"f"+i_forms;
-					this_form.prop("id", temp_form_id);
-				} else {
-					var temp_form_id = this_form.prop("id");
-				}
-				var temp_form_table_id = this_form.data("table_index");
-				if (typeof(temp_form_table_id) === "undefined") {
-					this_form.data("table_index", this_table);
-				} else {
-					this_form.data("table_index", $("#"+temp_form_table_id));
-				}
-				this_form.on("submit", function(e) {
-					e.preventDefault();
-					_self.updateTable(raw,$(this).data("table_index"));
-				});
+				var temp_conf_output = this.configureForm(this_form,this_table,i_forms);
 			}
 			_tables["id"][temp_table_id]["forms"] = temp_form_cache;
-			console.log(temp_pagination_cache);
+
 			if (temp_pagination_cache.length < 1) {
 				var temp_this_pagination_output = this.configurePages(_tables["id"][temp_table_id],temp_table_id+"p0", _result[0].PAGE_TOTAL, _tables["id"][temp_table_id].page_number);
 				if (temp_this_pagination_output !== false) {
@@ -159,10 +152,10 @@ var SearchModule = {};
 			} else {
 				for (var i_pagination = 0; i_pagination < temp_pagination_cache.length; i_pagination++) {
 					var this_pagination = $(temp_pagination_cache[i_pagination]);
-					console.log(this_pagination);
-					console.log(this_table);
 					if (this_pagination.prop("id") == "") {
-						this_pagination.prop("id", temp_table_id+"p"+i_pagination);
+						var temp_pagination_id = this.incrementID(temp_table_id+"p"+i_pagination);
+						this_pagination.prop("id", temp_pagination_id);
+						
 					}
 					var temp_this_pagination_output = this.configurePages(_tables["id"][temp_table_id],this_pagination.prop("id"), _result[0].PAGE_TOTAL, _tables["id"][temp_table_id].page_number);
 					if (temp_this_pagination_output !== false) {
@@ -175,6 +168,39 @@ var SearchModule = {};
 		console.log(_tables);
 	}
 
+	this.configureHeader = function(input_header_jquery) {
+		var temp_header_output = {"column_index": input_header_jquery.data("column_index"), "link": input_header_jquery.data("link"), "link_URI": input_header_jquery.data("link_uri"), "link_index": input_header_jquery.data("link_index")};
+		return temp_header_output;
+	}
+
+	this.configureForm = function(input_form_jquery, input_table_root, input_form_number) {
+		if (typeof(input_form_jquery) === "undefined") {
+			return false;
+		}
+		if (typeof(input_table_root) === "undefined") {
+			return false;
+		}
+		var temp_table_id = input_table_root.id;
+		if (input_form_jquery.prop("id") == "") {
+			var temp_form_id = temp_table_id+"f"+input_form_number;
+			temp_form_id = this.incrementID(temp_form_id);
+			input_form_jquery.prop("id", temp_form_id);
+		} else {
+			var temp_form_id = input_form_jquery.prop("id");
+		}
+		var temp_form_table_id = input_form_jquery.data("table_index");
+		if (typeof(temp_form_table_id) === "undefined") {
+			input_form_jquery.data("table_index", input_table_root);
+		} else {
+			input_form_jquery.data("table_index", $("#"+temp_form_table_id));
+		}
+		input_form_jquery.on("submit", function(e) {
+			e.preventDefault();
+			_self.updateTable(raw[1],$(this).data("table_index"));
+		});
+		return input_form_jquery;
+	}
+
 	this.configurePages = function(input_table_root, input_pagination_id,input_pages_total, input_pages_current) {
 		if (input_table_root.search_URI == "" || typeof(input_table_root.search_URI) == "undefined") {
 			return false;
@@ -183,7 +209,7 @@ var SearchModule = {};
 		var temp_pagination_output_status = true;
 		var temp_pagination_output = $(this_table.DOM_object.find("#"+input_pagination_id));
 		if (temp_pagination_output.length < 1) {
-			temp_pagination_output_status = false;
+			temp_pagination_output_status = false; //element does not exist
 			var temp_pagination_id = this.incrementID(input_pagination_id);
 			var temp_pagination_output = $("<div>").prop("id", temp_pagination_id);
 		}
@@ -209,12 +235,14 @@ var SearchModule = {};
 	
 	this.incrementID = function(input_increment_id) {
 		if ($("#"+input_increment_id).length !== 0) {
-			var temp_input_numbers = input_increment_id.match(/\d+/g).map(Number);
-(temp_input_numbers);
-			var temp_increment_id = input_increment_id.split(temp_input_numbers)[0]+((Number(temp_input_numbers))++);
-			//var temp_increment_id = input_increment_id+($("#"+input_increment_id).length);
-			console.log(input_increment_id);
-			console.log(temp_increment_id);
+			var temp_input_numbers = input_increment_id.match(/\d+/g);
+			if (temp_input_numbers !== null) {
+				var temp_input_numbers_last = (temp_input_numbers.length -1);
+				temp_input_numbers = temp_input_numbers.map(Number)[temp_input_numbers_last];
+				var temp_increment_id = input_increment_id.split(temp_input_numbers)[0]+(temp_input_numbers+1);
+			} else {
+				temp_increment_id = input_increment_id+"0"
+			}
 			return this.incrementID(temp_increment_id);
 		} else {
 			return input_increment_id;
